7b9feacdb4dedcd7e15e235e8ad1aca80a1c5701
hamedjahantigh-git <n.hamedjahantigh@gmail.com>
2024-06-13 16:12:01 +0330

    add some function to data_processor.py

--- data/data_processor.py
+++ data/data_processor.py
@@ -3,6 +3,10 @@ import requests
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
 """
 This class for crawl Taghche data for 
 complecation dataset    name of the author,
@@ -37,5 +41,75 @@ class Crawler:
             else:
                 print("Failed to retrieve the webpage url: "+ url)
         return [author_list, translator_list, publisher_list]
+
+"""
+This class do all process for data prepration.
+Dataframe should have column name date, comment,
+bookname, rate, bookID, and like 
+"""
+class DataProcessor:
+    _required_columns = ["date", "comment", "bookname", "rate", "bookID", "like"]
+    
+    def __init__(self, data: pd.DataFrame) -> None:
+        missing_columns = [col for col in self._required_columns if col not in data.columns]
+        if missing_columns:
+            raise Exception(f"DataFrame is missing required columns: {missing_columns}")
+        data.dropna(inplace=True)
+        data["rate"] = data["rate"].astype(int)
+        data["bookID"] = data["rate"].astype(int)
+        data["like"] = data["like"].astype(int)
+        data["id"] = data.index
+        self._data = data
+        self._balance_df = None
+
+    def get_df(self, balance_type = False) -> pd.DataFrame:
+        return self._balance_df if balance_type else self._data
+
+    def create_label(self, negative_boundary: int, positive_boundary: int) -> None:
+        def label_sentiment(score):
+            if score>=positive_boundary:
+                return 2 #Positive Label
+            elif score>negative_boundary:
+                return 1 #Neutral Label
+            else:
+                return 0 #Negetive Label
+        self._data["sentiment"] = self._data["rate"].apply(label_sentiment)
+        
+    def balance_data(self):
+        positive = self._data[self._data['sentiment'] == 2]
+        neutral = self._data[self._data['sentiment'] == 1]
+        negative = self._data[self._data['sentiment'] == 0]
+
+        # Find the size of the smallest class
+        min_size = min(len(positive), len(neutral), len(negative))
+        print("Positive Size: ", len(positive))
+        print("Neutral Size: ", len(neutral))
+        print("Negative Size: ", len(negative))
+        # sample to balance classes
+        positive = positive.iloc[:min_size]
+        neutral =  neutral.iloc[:min_size]
+        negative =  negative.iloc[:min_size]
+        # Combine sampled classes
+        self._balance_df =  pd.concat([positive, neutral, negative]).\
+            sample(frac=1, random_state=42, replace=True).reset_index(drop=True)
+
+
+    
+      
+
+class DataSpliter:
+    
+    def __init__(self, data: pd.DataFrame) -> None:
+        self._data = data
+    
+    #TODO @Amir    
+    def k_fold(self) -> List:
+        pass
+    
+    # create train, validation, and test dataset for torch learning
+    def dataset_train_val_test(self):
+        train_data, temp_data = train_test_split(self._, test_size=0.2,  shuffle=False, stratify=balanced_data['sentiment'])
+
+    
         
         
